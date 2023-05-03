35cebcd6b2d8836651f9ece4f2878b47baf52873
Chathura Lakshan <it21168536@my.sliit.lk>
2023-05-03 18:28:55 +0530

    dulsha Ui update

---
+++ app/src/main/java/com/example/myfirst/IT21357930PaymentDetails.kt
@@ -0,0 +1,11 @@
+package com.example.myfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IT21357930PaymentDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_it21357930_payment_details)
+    }
+}
